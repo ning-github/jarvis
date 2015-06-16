5c759c7be275d8d21ff71f0fa11bbbcd750336c1
Ning Xia <nxiamail@gmail.com>
2015-06-16 15:18:24 -0700

    Marvel API is giving a status OK 200 even when the thumbnail is not ready - using an if statment to prevent error

--- client/app/app.js
+++ client/app/app.js
@@ -19,8 +19,17 @@ angular.module('jarvis', ['ui.router'])
 })
 
 .controller('HeroesController', function($scope, Heroes){
+  $scope.pic = '';
   $scope.display = '';  // clear display before redisplaying
   $scope.search = function(hero){
+    $scope.newHero = ''; // clear search field
+
+    // because the Marvel API is buggy regarding thumbnails, use if conditional
+    if (done.data.results[0].thumbnail){
+      $scope.pic = done.data.results[0].thumbnail.path + '.' + done.data.results[0].thumbnail.extension;  
+    } else{
+      $scope.pic='';
+    }
     return Heroes.search(hero).then(function(done){
       return $scope.display = done.data.results[0].description;
     });
