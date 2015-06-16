9936dbf9b455fc6ac30061710cd7d014d7f61747
Ning Xia <nxiamail@gmail.com>
2015-06-16 02:34:56 -0700

    correcting for server-hashing/request-sending, rather than angular client

--- client/app/app.js
+++ client/app/app.js
@@ -1,21 +1,7 @@
-// to create an md5 hash for Marvel's serverside API use
-var crypto = require('crypto');
-// for getting API keys
-var config = require('config.js');
-
 angular.module('jarvis', ['ui.router'])
 
 .factory('Heroes', function($http){
   var search = function(heroName){
-    // for timestamp needed in hash
-    var ts = new Date().getTime();
-    // the url for the api call
-    var md5 = crypto.createHash('md5');
-    var hash = md5.update(ts + config.private_key + config.public_key).digest('hex');
-    var apiUrl = 'http://gateway.marvel.com:80/v1/public/characters?name='+heroName+'&apikey='+config.public_key;
-    apiUrl+='&ts='+ts+'&hash='+hash;
-    // log apiUrl
-    console.log('apiUrl: ', apiUrl);
     return $http({
       method: 'GET',
       url: apiUrl
--- server/server.js
+++ server/server.js
@@ -1,4 +1,21 @@
+// to create an md5 hash for Marvel's serverside API use
+var crypto = require('crypto');
+// for getting API keys
+var config = require('./config.js');
+
 var express = require('express');
+
+// server will need to handle request
+// for timestamp needed in hash
+var ts = new Date().getTime();
+// the url for the api call
+var md5 = crypto.createHash('md5');
+var hash = md5.update(ts + config.private_key + config.public_key).digest('hex');
+var apiUrl = 'http://gateway.marvel.com:80/v1/public/characters?name='+heroName+'&apikey='+config.public_key;
+apiUrl+='&ts='+ts+'&hash='+hash;
+// log apiUrl
+console.log('apiUrl: ', apiUrl);
+
 
 var app = express();
 var port = 3000;
