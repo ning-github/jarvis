a6a53630862e67b61123b10f471ebe8e83f59503
Ning Xia <nxiamail@gmail.com>
2015-06-16 01:09:13 -0700

    requiring config for API keys and crypto for Marvel's required hashed API access

--- client/app/app.js
+++ client/app/app.js
@@ -1,8 +1,13 @@
+// to create an md5 hash for Marvel's serverside API use
+var crypto = require('crypto');
+// for getting API keys
+var config = require('config.js');
+
 angular.module('jarvis', ['ui.router'])
 
 .factory('Heroes', function($http){
   var search = function(heroName){
-
+    
   };
   return {
     search: search
