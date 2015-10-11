dacc9dae0bb54ae02bfc0c49c2134ea282807c25
Ning Xia <nxiamail@gmail.com>
2015-10-11 15:37:32 -0700

    account for other API possibilities

--- server/config.example.js
+++ server/config.example.js
@@ -1,6 +1,7 @@
 var config = {
   'public-key': 'public_API_key_from_Marvel_Developer_Portal',
-  'private-key': 'private_API_key_from_Marvel_Developer_Portal'
+  'private-key': 'private_API_key_from_Marvel_Developer_Portal',
+  'vine_key': 'vine_key_from_comicvine'
 };
 
 module.exports = config;
